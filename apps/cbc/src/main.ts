cb5e181edf4ef590fda6b1407c9ebdd2ce514cca
Ilbie <beancurd2022@gmail.com>
2026-08-27 09:56:17 +0900

    chore: prepare v0.1.1-alpha.8 release

--- apps/cbc/src/main.ts
+++ apps/cbc/src/main.ts
@@ -6,7 +6,7 @@ import { createBunHost } from "./bun-host.ts";
 import { CliError, EXIT, type ExitCode } from "./exit.ts";
 import { route } from "./router.ts";
 
-export const CBC_VERSION = "0.1.1-alpha.7";
+export const CBC_VERSION = "0.1.1-alpha.8";
 
 export async function main(argv: readonly string[]): Promise<ExitCode> {
   const host = createBunHost(CBC_VERSION);
